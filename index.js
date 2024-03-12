53b010289ec0092d11e984e54c219f958dc15e18
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2024-03-12 20:38:21 +0100

    .

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import {BaseComponent} from "declarify-ui"
-import {declarify, DeclarifyScopeManager} from "declarify"
-const MyComponent =declarify(class extends BaseComponent{
-   constructor() {
-       super();
-       this.append('Hello, world!')
-   }
+import {declarify, declarifyKeep, DeclarifyScopeManager} from "declarify"
+
+const MyComponent = declarify(class extends BaseComponent {
+    counter = declarifyKeep(0)
+
+    constructor() {
+        super();
+        this.append('Hello, world!')
+        this.append(this.counter.value)
+    }
+
+    onclick() {
+        console.log('clicked')
+        this.counter.value++;
+    }
 });
 DeclarifyScopeManager.enterScope(Symbol())
 document.body.append(new MyComponent().toDOMNode())
